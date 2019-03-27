c7472d3805e0df033463da7a091e46e89c9bc2d7
Steve Golden <sgolden2525@outlook.com>
2019-03-26 20:06:10 -0500

    Added employee route
    Added route within employeeController.js
    
    Linked employeeController.js to server.js with require.
    
    app.use middleware to link the employeeController.js route to /employee route

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 require('./models/db');
 
 const express = require('express');
-const app = express();
+
+const employeeController = require('./controllers/employeeController');
+
+var app = express();
 
 app.listen(3000, () => {
   console.log('Express server started at port: 3000')
 });
+
+app.use('/employee', employeeController);
